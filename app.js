6ee8b4f3e2aa3e26f3a22561adf9240895e94a6f
taxpayer131 <chris.stefan1844@gmail.com>
2017-05-09 20:28:57 -0700

    first wave of lab2 code

---
+++ app.js
@@ -0,0 +1,38 @@
+<script>
+  'use strict';
+
+  var gender = prompt('Yes or no - are you a male?');
+  console.log('gender:' + gender);
+
+  if (answer === 'N') {
+    alert('I agree.');
+  } else {
+    alert('That is not what your mom said.')
+          }
+
+  var answer = prompt(user + ', Please answer with Y or N.  Does this suit make
+   me look fat?');
+  console.log('users answer:' + answer);
+
+  if (answer === 'N') {
+    alert('Liar. My face does that.');
+  } else {
+    alert('That's okay. Everyone knows that suits add fifty pounds.')
+  }
+
+  var height = prompt('Can you guess, Y or N, how tall I am?');
+  console.log('height:' + height);
+
+  if (answer === 'N') {
+    alert('Then open your eyes and try.');
+  } else {
+    alert('You must be a rocket scientist.')
+
+  var hobby = prompt('Do you own a car - Y or N?');
+  console.log('hobby:' + hobby);
+
+  if (answer === 'N') {
+    alert('You must be some kind of treehugger.');
+  } else {
+    alert('You are killing the planet.')
+  </script>
